d866dc9b21f25fc1f3824dd40366321feb7e1f28
notXX <notxx@qq.com>
2016-12-06 20:19:46 +0800

    fix problems & run unit tests

---
+++ dist/index.js
@@ -0,0 +1,277 @@
+"use strict";
+Promise.prototype.asCallback = function asCallback(callback, options) {
+    if (typeof callback !== "function")
+        return this;
+    return this.then((result) => {
+        if (Array.isArray(result) && options && options.spread) {
+            callback.apply(this, [null].concat(result));
+        }
+        else {
+            callback(null, result);
+        }
+        return result;
+    }).catch((e) => callback(e));
+};
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+function defaultSerializeFunction(session) {
+    let obj = {};
+    let prop;
+    for (prop in session) {
+        if (prop === "cookie") {
+            obj.cookie = session.cookie.toJSON ? session.cookie.toJSON() : session.cookie;
+        }
+        else {
+            obj[prop] = session[prop];
+        }
+    }
+    return obj;
+}
+function computeTransformFunctions(options, defaultStringify) {
+    if (options.serialize || options.unserialize) {
+        return {
+            serialize: options.serialize || defaultSerializeFunction,
+            unserialize: options.unserialize || (x => x),
+        };
+    }
+    if (options.stringify === false || defaultStringify === false) {
+        return {
+            serialize: defaultSerializeFunction,
+            unserialize: x => x,
+        };
+    }
+    if (options.stringify === true || defaultStringify === true) {
+        return {
+            serialize: JSON.stringify,
+            unserialize: JSON.parse,
+        };
+    }
+}
+module.exports = function connectMongo(connect) {
+    const Store = connect.Store || connect.session.Store;
+    const MemoryStore = connect.MemoryStore || connect.session.MemoryStore;
+    class MongoStore extends Store {
+        constructor(options) {
+            options = options || {};
+            if (options.fallbackMemory && MemoryStore) {
+                return new MemoryStore();
+            }
+            super(options);
+            this.ttl = options.ttl || 1209600;
+            this.collectionName = options.collection || "sessions";
+            this.autoRemove = options.autoRemove || "native";
+            this.autoRemoveInterval = options.autoRemoveInterval || 10;
+            this.transformFunctions = computeTransformFunctions(options, true);
+            this.options = options;
+            this.changeState("init");
+            const newConnectionCallback = (err, db) => {
+                if (err) {
+                    this.connectionFailed(err);
+                }
+                else {
+                    this.handleNewConnectionAsync(db);
+                }
+            };
+            if (options.url) {
+                MongoClient.connect(options.url, options.mongoOptions || {}, newConnectionCallback);
+            }
+            else if (options.mongooseConnection) {
+                if (options.mongooseConnection.readyState === 1) {
+                    this.handleNewConnectionAsync(options.mongooseConnection.db);
+                }
+                else {
+                    options.mongooseConnection.once("open", () => this.handleNewConnectionAsync(options.mongooseConnection.db));
+                }
+            }
+            else if (options.db && options.db.listCollections) {
+                if (options.db.openCalled || options.db.openCalled === undefined) {
+                    this.handleNewConnectionAsync(options.db);
+                }
+                else {
+                    options.db.open(newConnectionCallback);
+                }
+            }
+            else if (options.dbPromise) {
+                options.dbPromise
+                    .then(db => this.handleNewConnectionAsync(db))
+                    .catch(err => this.connectionFailed(err));
+            }
+            else {
+                throw new Error("Connection strategy not found");
+            }
+            this.changeState("connecting");
+        }
+        connectionFailed(err) {
+            this.changeState("disconnected");
+            throw err;
+        }
+        handleNewConnectionAsync(db) {
+            this.db = db;
+            return this
+                .setCollection(db.collection(this.collectionName))
+                .setAutoRemoveAsync()
+                .then(() => this.changeState("connected"));
+        }
+        setAutoRemoveAsync() {
+            let removeQuery = { expires: { $lt: new Date() } };
+            switch (this.autoRemove) {
+                case "native":
+                    return this.collection.createIndex({ expires: 1 }, { expireAfterSeconds: 0 });
+                case "interval":
+                    this.timer = setInterval(() => this.collection.remove(removeQuery, { w: 0 }), this.autoRemoveInterval * 1000 * 60);
+                    this.timer.unref();
+                    return Promise.resolve();
+                default:
+                    return Promise.resolve();
+            }
+        }
+        changeState(newState) {
+            if (newState !== this.state) {
+                this.state = newState;
+                this.emit(newState);
+            }
+        }
+        setCollection(collection) {
+            if (this.timer) {
+                clearInterval(this.timer);
+            }
+            this.collectionReadyPromise = undefined;
+            this.collection = collection;
+            return this;
+        }
+        collectionReady() {
+            let promise = this.collectionReadyPromise;
+            if (!promise) {
+                promise = new Promise((resolve, reject) => {
+                    switch (this.state) {
+                        case "connected":
+                            resolve(this.collection);
+                            break;
+                        case "connecting":
+                            this.once("connected", () => resolve(this.collection));
+                            break;
+                        case "disconnected":
+                            reject(new Error("Not connected"));
+                            break;
+                    }
+                });
+                this.collectionReadyPromise = promise;
+            }
+            return promise;
+        }
+        computeStorageId(sessionId) {
+            if (this.options.transformId && typeof this.options.transformId === "function") {
+                return this.options.transformId(sessionId);
+            }
+            else {
+                return sessionId;
+            }
+        }
+        get(sid, callback) {
+            return this.collectionReady()
+                .then(collection => collection.findOne({
+                _id: this.computeStorageId(sid),
+                $or: [
+                    { expires: { $exists: false } },
+                    { expires: { $gt: new Date() } },
+                ],
+            }))
+                .then(session => {
+                if (session) {
+                    const s = this.transformFunctions.unserialize(session.session);
+                    if (this.options.touchAfter > 0 && session.lastModified) {
+                        s.lastModified = session.lastModified;
+                    }
+                    this.emit("get", sid);
+                    return s;
+                }
+            })
+                .asCallback(callback);
+        }
+        set(sid, session, callback) {
+            if (this.options.touchAfter > 0 && session && session.lastModified) {
+                delete session.lastModified;
+            }
+            let s;
+            try {
+                s = { _id: this.computeStorageId(sid), session: this.transformFunctions.serialize(session) };
+            }
+            catch (err) {
+                return callback(err);
+            }
+            if (session && session.cookie && session.cookie.expires) {
+                s.expires = new Date(session.cookie.expires);
+            }
+            else {
+                s.expires = new Date(Date.now() + this.ttl * 1000);
+            }
+            if (this.options.touchAfter > 0) {
+                s.lastModified = new Date();
+            }
+            return this.collectionReady()
+                .then(collection => collection.update({ _id: this.computeStorageId(sid) }, s, { upsert: true }))
+                .then(writeOpResult => {
+                const rawResponse = writeOpResult.result;
+                if (rawResponse && rawResponse.upserted) {
+                    this.emit("create", sid);
+                }
+                else {
+                    this.emit("update", sid);
+                }
+                this.emit("set", sid);
+            })
+                .asCallback(callback);
+        }
+        touch(sid, session, callback) {
+            const updateFields = {}, touchAfter = this.options.touchAfter * 1000, lastModified = session.lastModified ? session.lastModified.getTime() : 0, currentDate = new Date();
+            if (touchAfter > 0 && lastModified > 0) {
+                const timeElapsed = currentDate.getTime() - lastModified;
+                if (timeElapsed < touchAfter) {
+                    return callback();
+                }
+                else {
+                    updateFields.lastModified = currentDate;
+                }
+            }
+            if (session && session.cookie && session.cookie.expires) {
+                updateFields.expires = new Date(session.cookie.expires);
+            }
+            else {
+                updateFields.expires = new Date(Date.now() + this.ttl * 1000);
+            }
+            return this.collectionReady()
+                .then(collection => collection.update({ _id: this.computeStorageId(sid) }, { $set: updateFields }))
+                .then(writeOpResult => {
+                if (writeOpResult.result.nModified === 0) {
+                    throw new Error("Unable to find the session to touch");
+                }
+                else {
+                    this.emit("touch", sid);
+                }
+            })
+                .asCallback(callback);
+        }
+        destroy(sid, callback) {
+            return this.collectionReady()
+                .then(collection => collection.remove({ _id: this.computeStorageId(sid) }))
+                .then(() => this.emit("destroy", sid))
+                .asCallback(callback);
+        }
+        length(callback) {
+            return this.collectionReady()
+                .then(collection => collection.count({}))
+                .asCallback(callback);
+        }
+        clear(callback) {
+            return this.collectionReady()
+                .then(collection => collection.drop())
+                .asCallback(callback);
+        }
+        close() {
+            if (this.db) {
+                this.db.close();
+            }
+        }
+    }
+    return MongoStore;
+};
